e2825e5bb022773db6b10f100e53bbdd7d7ce77e
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2024-07-08 20:59:15 -0700

    revise

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,23 +1,55 @@
 import Gig from "../models/gigSchema.js";
 import Order from "../models/orderSchema.js";
-export async function createOrder(req, res, next) {
-  try {
-    const gig = await Gig.findById(req.params.gigId);
-    const newOrder = new Order({
-      gigId: gig._id,
-      title: gig.title,
-      image: gig.cover,
-      price: gig.price,
-      buyerId: req.userId,
-      sellerId: gig.userId,
-      payment_intent: "Temporary",
-    });
-    const savedOrder = await newOrder.save();
-    res.status(201).send("successful");
-  } catch (err) {
-    next(err);
-  }
+
+import Stripe from "stripe";
+
+export async function intent(req, res, next) {
+  const gig = await Gig.findById(req.params.id);
+  const stripe = new Stripe(process.env.STRIPE_KEY);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: gig.price * 100,
+    currency: "cad",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  const newOrder = new Order({
+    gigId: gig._id,
+    title: gig.title,
+    image: gig.cover,
+    price: gig.price,
+    buyerId: req.userId,
+    sellerId: gig.userId,
+    payment_intent: paymentIntent.id,
+  });
+  const savedOrder = await newOrder.save();
+
+  res.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
 }
+
+// export async function createOrder(req, res, next) {
+//   try {
+//     const gig = await Gig.findById(req.params.gigId);
+//     const newOrder = new Order({
+//       gigId: gig._id,
+//       title: gig.title,
+//       image: gig.cover,
+//       price: gig.price,
+//       buyerId: req.userId,
+//       sellerId: gig.userId,
+//       payment_intent: "Temporary",
+//     });
+//     const savedOrder = await newOrder.save();
+//     res.status(201).send("successful");
+//   } catch (err) {
+//     next(err);
+//   }
+// }
+
 export async function getOrders(req, res, next) {
   try {
     const orders = await Order.find({
@@ -25,6 +57,24 @@ export async function getOrders(req, res, next) {
       isCompleted: true,
     });
     res.status(201).send(orders);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function confirm(req, res, next) {
+  try {
+    const order = await Order.findOneAndUpdate(
+      {
+        payment_intent: req.body.payment_intent,
+      },
+      {
+        $set: {
+          isCompleted: true,
+        },
+      }
+    );
+    res.status(201).send("Order has been confirmed.");
   } catch (err) {
     next(err);
   }
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middleWares/jwt.js";
-import { createOrder, getOrders } from "../controllers/orderController.js";
+import { getOrders, intent,confirm } from "../controllers/orderController.js";
 
 const router = express.Router();
 
-router.post("/:gigId", verifyToken, createOrder);
+// router.post("/:gigId", verifyToken, createOrder);
 router.get("/", verifyToken, getOrders);
+router.post("/create-payment-intent/:id", verifyToken, intent);
+router.put("/", verifyToken, confirm);
 
 export default router;
